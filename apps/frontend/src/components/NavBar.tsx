51ba2b26bb808719abaeaec4502f6c72bc3cb264
Dimitar Danailov <dimityr.danailov@gmail.com>
2023-06-23 13:54:13 +0300

    The application has a basic navigation

--- apps/frontend/src/components/NavBar.tsx
+++ apps/frontend/src/components/NavBar.tsx
@@ -3,8 +3,9 @@ import Link from 'next/link'
 export default function NavBar() {
   return (
     <nav>
+      <Link href="/">Home</Link>
+      <Link href="/transactions">Transactions</Link>
       <Link href="/dashboard">Dashboard</Link>
-      <Link href="/about">Dashboard</Link>
     </nav>
   )
 }
