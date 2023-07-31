8fa5e81a5b7348dff7420fd7ea9847309b3dfb19
Dimitar Danailov <dimityr.danailov@gmail.com>
2023-07-31 10:18:55 +0300

    fix: helper methods

--- apps/website/src/helpers/index.tsx
+++
@@ -1,4 +0,0 @@
-import createMimeTypeAllowedFiles from './create-mime-type-allowed-files'
-import filterAttachments from './filter-attachments'
-
-export {createMimeTypeAllowedFiles, filterAttachments}
