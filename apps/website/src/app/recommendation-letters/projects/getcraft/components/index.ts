8332bffb4b25dccf49b2461960f42efeb75b7c3c
Dimitar Danailov <dimityr.danailov@gmail.com>
2023-10-27 09:15:57 +0300

    Ajeng is part of the recommendation list

--- apps/website/src/app/recommendation-letters/projects/getcraft/components/index.ts
+++ apps/website/src/app/recommendation-letters/projects/getcraft/components/index.ts
@@ -2,5 +2,6 @@ import AlexLetter from './AlexLetter'
 import FannyLetter from './FannyLetter'
 import BayuLetter from './BayuLetter'
 import MarselLetter from './MarselLetter'
+import AjengLetter from './AjengLetter'
 
-export {AlexLetter, FannyLetter, BayuLetter, MarselLetter}
+export {AlexLetter, FannyLetter, BayuLetter, MarselLetter, AjengLetter}
--- apps/website/src/app/recommendation-letters/projects/getcraft/index.tsx
+++ apps/website/src/app/recommendation-letters/projects/getcraft/index.tsx
@@ -10,7 +10,13 @@ import {
   Position,
 } from '../../components'
 
-import {AlexLetter, FannyLetter, BayuLetter, MarselLetter} from './components'
+import {
+  AlexLetter,
+  FannyLetter,
+  BayuLetter,
+  MarselLetter,
+  AjengLetter,
+} from './components'
 
 const Letter: FC = () => {
   return (
@@ -24,22 +30,7 @@ const Letter: FC = () => {
       <FannyLetter />
       <BayuLetter />
       <MarselLetter />
-      <div>
-        Mitco, is a unique name for us Indonesian to pronounce, yet his tech
-        skill is extra ordinary. He is good a communicator and a knowledgeable
-        person that I can always count on to look for more information, always
-        available when in need. During my time working with him, he help me
-        understand the current system architecture and also teach me about
-        containers and container orchestration.
-      </div>
-      <div>
-        Dimitar is an expert programmer & skilful executor. He always keeps
-        up-to-date about the latest techniques in development & dedicated to
-        providing quality work. His energy is unlimited and his enthusiasm
-        motivates everyone on his team (including me!). I believe that he can
-        meet any challenge & I believe everyone would be exceedingly pleased
-        with his work.
-      </div>
+      <AjengLetter />
     </>
   )
 }
