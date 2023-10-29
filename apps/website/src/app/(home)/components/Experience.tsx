cc5f984fc84d7d15cff3b52915f302b5e5143ed3
Dimitar Danailov <dimityr.danailov@gmail.com>
2023-10-29 05:00:59 +0200

    Experience is part of the project

---
+++ apps/website/src/app/(home)/components/Experience.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+const Experience = () => {
+  return (
+    <ul>
+      <li>Contractor, Collinson group: August - present (Team size: 20+)</li>
+      <li>
+        Blockchain Architect, Siemens Energy: March - June (2023) (Team size:
+        15+);
+      </li>
+      <li>
+        Fullstack JS DevOps Architect, The international red cross: 2022 - 2023;
+        (Team size: 8);
+      </li>
+      <li>
+        BlockChain Staff Engineer, Propine.com: Sep, 2021 - August, 2022; (Team
+        size: 20+);
+      </li>
+      <li>
+        Software Engineering Manager, Elephant stocks: Sep, 2021 - 2022; (Team
+        size: 20+);
+      </li>
+      <li>
+        Full Stack Software Architect, Mentormate: 2015 - 2020; (Team size:
+        100+);
+      </li>
+      <li>CTO, Woollow Soft: 2014 - 2015; (Team size: 30+);</li>
+      <li>Developer and Team Leader: 2010 - 2014; (Team size: 20+);</li>
+    </ul>
+  )
+}
+
+export default Experience
