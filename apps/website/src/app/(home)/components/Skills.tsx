7427a0e5d66aa5aeadc8720f84357cd201c4d97d
Dimitar Danailov <dimityr.danailov@gmail.com>
2023-10-29 04:54:07 +0200

    Skills is added in the project folder

---
+++ apps/website/src/app/(home)/components/Skills.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import {FC} from 'react'
+
+const Skills: FC = () => {
+  return (
+    <ul>
+      <li>Nodejs: Express, Hapi, Nestsjs, etherjs, web3js</li>
+      <li>Nextjs, Vercel, Redux, GraphQL;</li>
+      <li>AWS, Azure, Google Cloud;</li>
+      <li>
+        Blockchain: Solidity, Etherium, Algorane, Cosmos, Elrond, Polygon;
+      </li>
+      <li>
+        Database Systems: PostgreSQL, MySQL, MSSQL, NoSQL, MongoDB, OpenSearch,
+        Elastic Search;
+      </li>
+      <li>
+        Containers and orchestration: Docker, Kubernetes, OpenShift, Vagrant,
+        TerraForm;
+      </li>
+    </ul>
+  )
+}
+
+export default Skills
