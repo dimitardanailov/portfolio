be4c46a30f43b33c7e9e19c993453b6356a70b11
Dimitar Danailov <dimityr.danailov@gmail.com>
2023-10-29 04:58:03 +0200

    Accomplishments is part of the project

---
+++ apps/website/src/app/(home)/components/Accomplishments.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+const Accomplishments = () => {
+  return (
+    <>
+      <ul>
+        <li>
+          13+ years experience. Portfolio includes clients located: USA,
+          Singapore, Israel, Germany, Great Britain, Switzerland, United Arab
+          Emirates, Indonesia;
+        </li>
+        <li>Ex - contractor Siemens and The international Red cross;</li>
+        <li>Ex CTO, Staff Engineer, Architect and Engineering manager;</li>
+        <li>Management and Leadership skills: 10+ years;</li>
+        <li>Top 4% user in Stack Overflow;</li>
+      </ul>
+    </>
+  )
+}
+
+export default Accomplishments
