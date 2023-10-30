3d4eeecb8e5f7baea8953e7d308059459c0ca7af
Dimitar Danailov <dimityr.danailov@gmail.com>
2023-10-30 08:49:35 +0200

    The project supports redirects

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {},
+  async redirects() {
+    return [
+      {
+        source: '/about',
+        destination: '/recommendation-letters',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
