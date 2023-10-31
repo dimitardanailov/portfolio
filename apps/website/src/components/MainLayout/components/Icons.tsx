b8c88fdc1c7d5c5158df926311f70b2902dd78b7
Dimitar Danailov <dimityr.danailov@gmail.com>
2023-10-31 09:47:11 +0200

    react-icons are part from the application. Application supports Github and Stackoverflow icon

--- apps/website/src/components/MainLayout/components/Icons.tsx
+++ apps/website/src/components/MainLayout/components/Icons.tsx
@@ -2,16 +2,47 @@
 
 import {FC} from 'react'
 
+import Link from 'next/link'
+
+import {FaGithub, FaStackOverflow} from 'react-icons/fa'
+
 import HFlexBox from '@/styled-components/Grid/HFlexBox'
 
 import styled from 'styled-components'
 
 const Container = styled(HFlexBox)`
-  margin: 1rem 0.4rem;
+  justify-content: center;
+  width: 100%;
+
+  margin: 1rem 0;
+`
+
+const InternalContainer = styled(HFlexBox)`
+  justify-content: space-around;
+  width: 80px;
 `
 
 const Icons: FC = () => {
-  return <Container>{'Hello world'}</Container>
+  return (
+    <Container>
+      <InternalContainer>
+        <Link
+          href="https://github.com/dimitardanailov"
+          target="_blank"
+          title="Please review my github profile"
+        >
+          <FaGithub size={32} />
+        </Link>
+        <Link
+          href="https://stackoverflow.com/users/609707/d-danailov"
+          target="_blank"
+          title="Please review my stackoverflow profile"
+        >
+          <FaStackOverflow size={32} />
+        </Link>
+      </InternalContainer>
+    </Container>
+  )
 }
 
 export default Icons
