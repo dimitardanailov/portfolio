9907973f90001b4dacf3a64abf110cea6059f53c
Dimitar Danailov <dimityr.danailov@gmail.com>
2023-11-17 05:59:54 +0200

    fix: rel.canonical

--- apps/website/src/app/(home)/layout.tsx
+++ apps/website/src/app/(home)/layout.tsx
@@ -4,7 +4,7 @@ import generateMetadata, {InternalMetadata} from '@/seo/metadata'
 const title = '13 years of helping build an inclusive web2 or web3 projects'
 const description =
   'Deep understanding for upcoming technologies to solve complex business problems;'
-const canonical = '/recommendation-letters'
+const canonical = ''
 
 const props: InternalMetadata = {
   title,
