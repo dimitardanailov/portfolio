15001ac8097cf69f9a3a1445f1557f3ce4381188
Dimitar Danailov <dimityr.danailov@gmail.com>
2023-11-18 19:39:41 +0200

    Analytics is part of layout

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import {Metadata} from 'next'
 
+import {Analytics} from '@vercel/analytics/react'
+
 import {Inter} from 'next/font/google'
 
 import '@/styles/global.css'
@@ -58,7 +60,10 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
