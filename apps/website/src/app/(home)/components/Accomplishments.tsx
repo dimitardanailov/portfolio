19f1e15dbb415f3fbc0b3ae4aa80cf3d1eb174a7
Dimitar Danailov <dimityr.danailov@gmail.com>
2023-11-19 20:47:17 +0200

    Accomplishments v2

--- apps/website/src/app/(home)/components/Accomplishments.tsx
+++ apps/website/src/app/(home)/components/Accomplishments.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import TargetBlankLink from '@/components/TargetBlankLink'
 import {listStyle} from './shared'
 
 const Accomplishments = () => {
@@ -8,14 +9,20 @@ const Accomplishments = () => {
       <div>Accomplishments</div>
       <ul className={listStyle}>
         <li>
-          13+ years experience. Portfolio includes clients located: USA,
+          13+ years experience. My portfolio includes clients located: USA,
           Singapore, Israel, Germany, Great Britain, Switzerland, United Arab
           Emirates, Indonesia;
         </li>
         <li>Ex - contractor Siemens and The international Red cross;</li>
         <li>Ex CTO, Staff Engineer, Architect and Engineering manager;</li>
         <li>Management and Leadership skills: 10+ years;</li>
-        <li>Top 4% user in Stack Overflow;</li>
+        <li>
+          <TargetBlankLink
+            text="Top 4% user in Stack Overflow"
+            title="Top 4% user in Stack Overflow"
+            href="https://stackoverflow.com/users/609707/d-danailov"
+          ></TargetBlankLink>
+        </li>
       </ul>
     </>
   )
