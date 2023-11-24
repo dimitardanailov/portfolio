cdab7e96e930a2fc4cca6b88fbcf9d2788cc2b69
Dimitar Danailov <dimityr.danailov@gmail.com>
2023-11-24 11:22:36 +0200

    source code cleanup

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -7,9 +7,7 @@ const withMDX = require('@next/mdx')()
 
 const nextConfig = {
   env: {},
-  // Configure `pageExtensions` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
   async redirects() {
     return [
       {
