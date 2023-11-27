8b97a5827bb93d156a5b007901e4fc6d97d0ade6
Dimitar Danailov <dimityr.danailov@gmail.com>
2023-11-27 09:36:45 +0200

    Linkedin is part of website

--- apps/website/src/components/MainLayout/components/Icons.tsx
+++ apps/website/src/components/MainLayout/components/Icons.tsx
@@ -4,7 +4,7 @@ import {FC} from 'react'
 
 import Link from 'next/link'
 
-import {FaGithub, FaStackOverflow} from 'react-icons/fa'
+import {FaGithub, FaStackOverflow, FaLinkedin} from 'react-icons/fa'
 
 import HFlexBox from '@/styled-components/Grid/HFlexBox'
 
@@ -19,26 +19,35 @@ const Container = styled(HFlexBox)`
 
 const InternalContainer = styled(HFlexBox)`
   justify-content: space-around;
-  width: 80px;
+  width: 120px;
 `
 
 const Icons: FC = () => {
+  const size = 32
+
   return (
     <Container>
       <InternalContainer>
         <Link
           href="https://github.com/dimitardanailov"
           target="_blank"
-          title="Please review my github profile"
+          title="Github profile"
         >
-          <FaGithub size={32} />
+          <FaGithub size={size} />
         </Link>
         <Link
           href="https://stackoverflow.com/users/609707/d-danailov"
           target="_blank"
-          title="Please review my stackoverflow profile"
+          title="Stackoverflow profile"
         >
-          <FaStackOverflow size={32} />
+          <FaStackOverflow size={size} />
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/ddanailov/"
+          target="_blank"
+          title="Linkedin profile"
+        >
+          <FaLinkedin size={size} />
         </Link>
       </InternalContainer>
     </Container>
