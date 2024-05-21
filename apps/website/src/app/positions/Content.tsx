b99dccccc2d327c2166d7dfe19be5d8b840d25dd
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-05-21 13:31:07 +0300

    The website has more information profile

--- apps/website/src/app/positions/Content.tsx
+++ apps/website/src/app/positions/Content.tsx
@@ -1,11 +1,15 @@
 'use client'
 
+import {Slogan} from '@/styled-components'
+
 import Collinson from './full-stack-engineer-collinson/Content'
 import SiemensEnergy from './blockchain-architect-siemens-energy/Content'
 import TheInternationRedCross from './fullstack-javascript-devops-architect/Content'
 import Propine from './blockchain-staff-engineer-propine/Content'
 import Elephantstock from './software-engineering-manager/Content'
 import GetCraft from './software-architect-getcraft/Content'
+
+import {experienceByYears} from '@/constants/text'
 
 import styled from 'styled-components'
 
@@ -23,6 +27,12 @@ const Divider = styled.span`
 const Content = () => {
   return (
     <>
+      <Slogan>
+        <span className="active-color">{experienceByYears} years</span> of{' '}
+        <span className="active-color">helping</span> build an inclusive{' '}
+        <span className="active-color">web2</span> or{' '}
+        <span className="active-color">web3</span> projects
+      </Slogan>
       <Collinson />
       <Divider />
       <SiemensEnergy />
