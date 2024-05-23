1bcdbc302e4642bd24c2def4d2c5d4b22b2632f8
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-05-23 06:36:48 +0300

    rename of the method name

--- apps/website/src/utils/coinbase/btcPairs.ts
+++ apps/website/src/utils/coinbase/btcPairs.ts
@@ -1,6 +1,6 @@
 import comparing from './comparing'
 
-export function btcPairIsSupportedByCoinbase(cryptoSymbol: string) {
+export function btcPairs(cryptoSymbol: string) {
   const supportedCoins = [
     'ALGORAND',
     'matic-network',
