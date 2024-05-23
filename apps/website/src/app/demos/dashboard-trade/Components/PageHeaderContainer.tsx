9ddd5a12f16b9d3368ffdebcd0337a15da848ca2
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-05-23 06:20:09 +0300

    PageHeaderCointainer is a reusable component

--- apps/website/src/app/demos/dashboard-trade/Components/PageHeaderContainer.tsx
+++ apps/website/src/app/demos/dashboard-trade/Components/PageHeaderContainer.tsx
@@ -1,1 +1,33 @@
 'use client'
+
+import styled from 'styled-components'
+
+import BasicButton from '@/styled-components/Buttons/BasicButton'
+
+const Wrapper = styled.div`
+  position: relative;
+
+  display: flex;
+  flex-direction: row;
+  flex-direction: center;
+  justify-content: space-end;
+  align-items: center;
+  width: 100%;
+  margin: 0 auto;
+`
+
+const StyledButton = styled(BasicButton)`
+  margin-left: auto;
+`
+
+const PageHeaderContainer = () => {
+  return (
+    <Wrapper>
+      <StyledButton type="button" fontSize="1.2rem" padding="1rem 2rem">
+        Refresh
+      </StyledButton>
+    </Wrapper>
+  )
+}
+
+export default PageHeaderContainer
