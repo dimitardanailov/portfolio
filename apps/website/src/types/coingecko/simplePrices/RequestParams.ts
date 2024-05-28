ca23736d2dda3a0684bf5dad1788dd3b54903c0e
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-05-28 06:14:07 +0300

    The project support RequestParams as a property

---
+++ apps/website/src/types/coingecko/simplePrices/RequestParams.ts
@@ -0,0 +1,7 @@
+export type CoingeckoSimplePriceRequest = {
+  ids: string
+  vs_currencies: string
+  include_24hr_change: boolean
+  include_7d_change: boolean
+  include_last_updated_at: boolean
+}
