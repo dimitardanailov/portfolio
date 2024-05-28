bbbb62235e4f4ccad18fe73ba7cbf3c754eefab6
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-05-28 06:56:48 +0300

    CoingeckoSimplePriceResponse is part of the setup

---
+++ apps/website/src/types/coingecko/simplePrices/ResponseParams.ts
@@ -0,0 +1,9 @@
+export type CoingeckoSimplePriceResponse = {
+  btc: number
+  btc_24h_change: number
+  cryptoCurrency: string
+  eth: number
+  eth_24h_change: number
+  usd: number
+  usd_24h_change: number
+}
