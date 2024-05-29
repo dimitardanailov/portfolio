fb8c25b9d7349bc8e525b14693a11ba115892493
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-05-29 09:23:47 +0300

    getPrices supports Promise

--- apps/website/src/app/demos/dashboard-trade/utils/getPrices.ts
+++ apps/website/src/app/demos/dashboard-trade/utils/getPrices.ts
@@ -1,52 +1,62 @@
 import axios from 'axios'
 
-function getPrices(setPrices: unknown) {
+import {
+  CoingeckoSimplePriceRequest,
+  CoingeckoSimplePriceResponse,
+} from '@/types/coingecko/simplePrices'
+
+function getPrices(
+  params: CoingeckoSimplePriceRequest,
+): Promise<Array<CoingeckoSimplePriceResponse>> {
   const domain = process.env.NEXT_PUBLIC_COINGECKO_DOMAIN
   const key = process.env.NEXT_PUBLIC_COINGECKO_API_KEY
 
   const url = `${domain}/simple/price`
 
-  const coingeckoIds = ['bitcoin,chainlink,cosmos,solana']
-
   const options = {
     method: 'GET',
     url,
-    params: {
-      ids: coingeckoIds.join(','),
-      vs_currencies: 'btc,usd,eth',
-      include_24hr_change: true,
-      include_7d_change: true,
-      include_last_updated_at: true,
-    },
+    params: params,
     headers: {
       accept: 'application/json',
       'x-cg-demo-api-key': key,
     },
   }
 
-  axios
-    .request(options)
-    .then(function (response) {
-      const keys = Object.keys(response.data)
+  const promise: Promise<Array<CoingeckoSimplePriceResponse>> = new Promise(
+    (resolve, reject) => {
+      axios
+        .request(options)
+        .then(function (response) {
+          const keys = Object.keys(response.data)
 
-      const coingeckoPrices = keys.map(key => {
-        const item = response.data[key]
-        return {
-          cryptoCurrency: key,
-          btc: item.btc,
-          usd: item.usd,
-          eth: item.eth,
-          btc_24h_change: item.btc_24h_change,
-          eth_24h_change: item.eth_24h_change,
-          usd_24h_change: item.usd_24h_change,
-        }
-      })
+          const coingeckoPrices: Array<CoingeckoSimplePriceResponse> = keys.map(
+            key => {
+              const item = response.data[key]
+              return {
+                cryptoCurrency: key,
+                btc: item.btc,
+                usd: item.usd,
+                eth: item.eth,
+                btc_24h_change: item.btc_24h_change,
+                eth_24h_change: item.eth_24h_change,
+                usd_24h_change: item.usd_24h_change,
+              }
+            },
+          )
 
-      console.log('coingeckoPrices', coingeckoPrices)
-    })
-    .catch(function (error) {
-      console.error('prices ...', error)
-    })
+          console.log('axios.prices', coingeckoPrices)
+
+          resolve(coingeckoPrices)
+        })
+        .catch(function (error) {
+          console.error('prices ...', error)
+          reject(error)
+        })
+    },
+  )
+
+  return promise
 }
 
 export default getPrices
