3fa9787a56805d4e773be4f5464cf04a11c5ca01
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-05-29 14:44:05 +0300

    Source code cleanup

--- apps/website/src/app/demos/dashboard-trade/Content.tsx
+++ apps/website/src/app/demos/dashboard-trade/Content.tsx
@@ -19,81 +19,12 @@ import {
   CoingeckoTableBody,
   PageHeaderContainer,
 } from './Components'
-import {
-  CoinCell,
-  CryptoIcon,
-  PriceUSDCell,
-  Table,
-  TableCell,
-  TableRow,
-} from './styled'
+
+import Table from './styled/Table/Table'
 
 const CustomAlert = styled(Alert)`
   position: relative;
 `
-
-const columnDimensions = {
-  coin: {
-    width: '8%',
-  },
-  usd: {
-    price: {
-      width: '8.5%',
-    },
-    priceChanged: {
-      width: '14%',
-    },
-  },
-  btc: {
-    price: {
-      width: '10.5%',
-    },
-    priceChanged: {
-      width: '24.25%',
-    },
-  },
-  eth: {
-    price: {
-      width: '10.5%',
-    },
-    priceChanged: {
-      width: '24.25%',
-    },
-  },
-}
-
-// @ts-ignore
-function renderRow({data, index, style}) {
-  const item = data.prices[index]
-  const usdChange = parseFloat(item['usd_24h_change']).toFixed(2)
-  var formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-  })
-  const price = formatter.format(parseFloat(item.usd))
-
-  const calcOldPrice = (currentPriceStr: string, percent: number) => {
-    const currentPrice = parseFloat(currentPriceStr)
-    let indexChange = Math.abs(percent) / 100
-    const dif = currentPrice * indexChange
-    const sign = Math.sign(percent) * -1
-
-    return currentPrice + sign * dif
-  }
-
-  const oldPriceUSD = calcOldPrice(item.usd, item['usd_24h_change'])
-  const oldPriceBTC = calcOldPrice(item.btc, item['btc_24h_change'])
-  const oldPriceETH = calcOldPrice(item.eth, item['eth_24h_change'])
-  const pairSupportedByCoinbase = {
-    eth: true,
-  }
-
-  const icon = `https://ddanailov.dev/_next/static/crypto/${item.cryptoCurrency}.svg`
-  const coinBaseIcon =
-    'https://ddanailov.dev/_next/static/crypto/coinbase-icon-symbol-1.svg'
-
-  return <>{item.cryptoCurrency}</>
-}
 
 const Content = () => {
   const [prices, setPrices] = useState<Array<CoingeckoSimplePriceResponse>>([])
