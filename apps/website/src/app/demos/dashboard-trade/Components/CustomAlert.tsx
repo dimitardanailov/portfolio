2f6dff8610c164697e15ab6b22ac4f36831910e3
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-05-30 09:22:39 +0300

    CustomAlert is a reusable component

---
+++ apps/website/src/app/demos/dashboard-trade/Components/CustomAlert.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import {FC} from 'react'
+
+import Alert from '@mui/material/Alert'
+
+import Sorting from '../types/Sorting'
+
+import styled from 'styled-components'
+
+const StyledAlert = styled(Alert)`
+  position: relative;
+  margin: 0.5rem;
+`
+
+export interface Props {
+  sorting: Sorting
+}
+
+const CustomAlert: FC<Props> = ({sorting}) => {
+  const SortElement = () => {
+    if (sorting.column === null) return null
+
+    return (
+      <span>
+        Sort by {sorting.column}, order by {sorting.orderBy}.
+      </span>
+    )
+  }
+
+  return (
+    <StyledAlert severity="success">
+      <span>The list with crypto coins has a new version! </span>
+      <SortElement />
+    </StyledAlert>
+  )
+}
+
+export default CustomAlert
