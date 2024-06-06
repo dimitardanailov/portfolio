c0a919f8772ac319aca8130aceffbfa15b362b78
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-06-06 08:56:15 +0300

    UICoinCellParams is supported

---
+++ apps/website/src/types/coingecko/cells/UICoinCellParams.ts
@@ -0,0 +1,9 @@
+import MobileSortingCellParams from './MobileSortingCellParams'
+
+type UICoinCellParams = {
+  label: string
+  width: string
+  mobile: MobileSortingCellParams
+}
+
+export default UICoinCellParams
