173fd9cb56279518181ae8a4e13d2015a4aceedb
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-06-06 08:57:28 +0300

    MobileSortingCellParams, UICellParams and UICoinCellParams are reusable items

---
+++ apps/website/src/types/coingecko/cells/index.ts
@@ -0,0 +1,5 @@
+import MobileSortingCellParams from './MobileSortingCellParams'
+import UICellParams from './UICellParams'
+import UICoinCellParams from './UICoinCellParams'
+
+export type {MobileSortingCellParams, UICellParams, UICoinCellParams}
