e5c795476aa818901c5213d3deb5aa58deced69a
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-06-14 08:29:26 +0300

    seo information is reusable

---
+++ apps/website/src/app/demos/dashboard-trade/seo.ts
@@ -0,0 +1,15 @@
+import {InternalMetadata} from '@/seo/metadata'
+
+const title = 'Coingecko: crypto monitoring dashboard'
+const description = 'Trading pairs are'
+const canonical = '/demos/dashboard-trade'
+
+const props: InternalMetadata = {
+  title,
+  description,
+  canonical,
+}
+
+export default props
+
+export {title, description, canonical}
