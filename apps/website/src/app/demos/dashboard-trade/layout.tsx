cf2662a37bd2e234531bc0b627da46a18a527ed2
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-06-14 08:31:11 +0300

    seo information is used by layout

--- apps/website/src/app/demos/dashboard-trade/layout.tsx
+++ apps/website/src/app/demos/dashboard-trade/layout.tsx
@@ -1,15 +1,6 @@
 import layout from '@/components/NextjsLayout'
-import generateMetadata, {InternalMetadata} from '@/seo/metadata'
-
-const title = 'Coingecko: crypto monitoring dashboard'
-const description = 'Trading pairs are'
-const canonical = '/demos/dashboard-trade'
-
-const props: InternalMetadata = {
-  title,
-  description,
-  canonical,
-}
+import generateMetadata from '@/seo/metadata'
+import props from './seo'
 
 export const metadata = generateMetadata(props)
 
