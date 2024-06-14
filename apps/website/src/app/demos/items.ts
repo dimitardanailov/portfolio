fd37fe3344ba431e41d1f9f0c019385fd9f4779f
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-06-14 08:35:46 +0300

    The list has all demos items

--- apps/website/src/app/demos/items.ts
+++ apps/website/src/app/demos/items.ts
@@ -1,5 +1,8 @@
-import dashboardEtheriumAirdrops from '@/app/demos/dashboard-etherium-airdrops/listItem'
+import etheriumAirdrops from '@/app/demos/dashboard-etherium-airdrops/listItem'
+import solanaAirdrops from '@/app/demos/dashboard-solana-airdrops/listItem'
+import dashboardTrade from '@/app/demos/dashboard-trade/listItem'
+import evmRestake from '@/app/demos/dashboard-etherium-restake/listItem'
 
-const items = [dashboardEtheriumAirdrops]
+const items = [dashboardTrade, evmRestake, etheriumAirdrops, solanaAirdrops]
 
 export default items
