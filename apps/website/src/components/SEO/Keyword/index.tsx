eab5b1a00107fd2ee09b4991375e479dcea8038a
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-06-18 16:55:34 +0300

    fix: styled components

--- apps/website/src/components/SEO/Keyword/index.tsx
+++ apps/website/src/components/SEO/Keyword/index.tsx
@@ -1,14 +1,11 @@
 import {FC} from 'react'
-import styled from 'styled-components'
-
-const Wrapper = styled.strong``
 
 interface Props {
   word: string
 }
 
 const Layout: FC<Props> = ({word}) => {
-  return <Wrapper>{word}</Wrapper>
+  return <strong>{word}</strong>
 }
 
 export default Layout
