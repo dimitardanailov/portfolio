7c8fa9e6ca24d94d5d8458ba7ef50933576376d9
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-06-24 11:15:09 +0300

    Google AI key is part of the configuration

--- apps/website/environment.d.ts
+++ apps/website/environment.d.ts
@@ -2,5 +2,6 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     readonly NEXT_PUBLIC_COINGECKO_DOMAIN: string
     readonly NEXT_PUBLIC_COINGECKO_API_KEY: string
+    readonly NEXT_PUBLIC_GOOGLE_AI_KEY: string
   }
 }
