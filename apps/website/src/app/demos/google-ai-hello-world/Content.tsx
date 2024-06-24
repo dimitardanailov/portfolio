64ca42b438b0cde25ea9de9ec5b41f48eea386ef
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-06-24 11:18:59 +0300

    Google AI boileplate is completed

---
+++ apps/website/src/app/demos/google-ai-hello-world/Content.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import {FC} from 'react'
+
+const Component: FC = () => {
+  return <>Hello world</>
+}
+
+export default Component
---
+++ apps/website/src/app/demos/google-ai-hello-world/seo.ts
@@ -0,0 +1,15 @@
+import {InternalMetadata} from '@/seo/metadata'
+
+const title = 'Google AI - Hello world app'
+const description = ''
+const canonical = '/demos/google-ai-hello-world'
+
+const props: InternalMetadata = {
+  title,
+  description,
+  canonical,
+}
+
+export default props
+
+export {title}
