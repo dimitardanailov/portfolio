f154ee0046e2d473ba1ef4eddefc84b628258dcf
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-06-24 11:52:42 +0300

    Application uses Gemini AI

--- apps/website/src/app/demos/google-ai-hello-world/Content.tsx
+++ apps/website/src/app/demos/google-ai-hello-world/Content.tsx
@@ -1,9 +1,40 @@
 'use client'
 
-import {FC} from 'react'
+import {FC, useState} from 'react'
+
+import {createGoogleGenerativeAI} from '@ai-sdk/google'
+import {generateText} from 'ai'
 
 const Component: FC = () => {
-  return <>Hello world</>
+  const [promtResponse, setPromtPresponse] = useState<string>('')
+
+  const generatePrompt = async () => {
+    if (promtResponse.length === 0) {
+      const google = createGoogleGenerativeAI({
+        apiKey: process.env.NEXT_PUBLIC_GOOGLE_AI_KEY,
+      })
+      const model = google('models/gemini-pro')
+
+      const {text} = await generateText({
+        model,
+        prompt: 'Write a vegetarian lasagna recipe for 4 people.',
+      })
+
+      setPromtPresponse(text)
+    }
+  }
+
+  return (
+    <>
+      <button onClick={generatePrompt} disabled={promtResponse.length > 0}>
+        Generate
+      </button>
+      <div>
+        <span>Write a vegetarian lasagna recipe for 4 people:</span>
+        <p>{promtResponse}</p>
+      </div>
+    </>
+  )
 }
 
 export default Component
