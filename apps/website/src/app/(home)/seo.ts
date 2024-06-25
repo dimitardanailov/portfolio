8a4c3910ecab323d8a865160d951651a6abea8ab
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-06-25 05:22:01 +0300

    Website uses constants and linkedin headline

---
+++ apps/website/src/app/(home)/seo.ts
@@ -0,0 +1,17 @@
+import {InternalMetadata} from '@/seo/metadata'
+import {experienceByYears} from '@/constants/text'
+
+const title = `${experienceByYears} years of helping build an inclusive web2 or web3 projects`
+const description =
+  'Full Stack Engineer with 14 Years Experience. Seeking Staff Engineer, Team Lead, or Architect Roles. Top 4% on Stack Overflow, Leadership roles: 10+ years'
+const canonical = ''
+
+const props: InternalMetadata = {
+  title,
+  description,
+  canonical,
+}
+
+export default props
+
+export {title}
