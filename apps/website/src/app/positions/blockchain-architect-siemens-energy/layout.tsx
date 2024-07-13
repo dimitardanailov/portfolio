659f222e1596f7b0b57b97505a385caf5399977f
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-07-13 06:29:26 +0300

    sufix is a reusable item

--- apps/website/src/app/positions/blockchain-architect-siemens-energy/layout.tsx
+++ apps/website/src/app/positions/blockchain-architect-siemens-energy/layout.tsx
@@ -1,14 +1,14 @@
 import layout from '@/components/NextjsLayout'
 import generateMetadata, {InternalMetadata} from '@/seo/metadata'
 
-import stack from './stack'
+import {sufix, technologies} from './db'
 
 const title = 'Full stack engineer, Collinson group'
-const description = `Technology stack (I'm happy to provide technical tips for each technology in the list): ${stack.join(
+const description = `Technology stack (I'm happy to provide technical tips for each technology in the list): ${technologies.join(
   ', ',
 )}`
 
-const canonical = '/positions/blockchain-architect-siemens-energy'
+const canonical = `/positions/${sufix}`
 
 const props: InternalMetadata = {
   title,
