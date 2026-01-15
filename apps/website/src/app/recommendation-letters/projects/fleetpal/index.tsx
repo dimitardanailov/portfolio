8e287ede568771e3c1a2e8fbe64ff356e333db44
Dimitar Danailov <dimityr.danailov@gmail.com>
2026-01-15 10:40:23 +0200

    mike image is added

--- apps/website/src/app/recommendation-letters/projects/fleetpal/index.tsx
+++ apps/website/src/app/recommendation-letters/projects/fleetpal/index.tsx
@@ -9,7 +9,10 @@ import {
   MarketPlace,
   Position,
   Technologies,
+  PersonInfo,
 } from '../../components'
+
+import source from '@/data/testimonials/images/fleetpal/mike'
 
 const Letter: FC = () => {
   return (
@@ -20,6 +23,7 @@ const Letter: FC = () => {
       <HeadQuater text="USA and Israel" />
       <Position text="Software Consultant" />
       <Technologies technologies="React, Nextjs, Vercel, GraphQL, Headless CMS" />
+      <PersonInfo name="Milko (Mike) Valnev" source={source} title="CEO" />
       <p>
         Dimitar is a very professional and experienced developer and consultant.
         His knowledge spreads across multiple industries, and it is not limited
